0b5a9c2b2e67ae5284ee15c937f2c0c6af90da15
Kerdwin Echeverria <kerdwin.echeverria@lan.smartechla.com>
2022-11-06 19:24:58 -0400

    primero

---
+++ principal.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, redirect, url_for,render_template
+
+app = Flask(__name__, static_folder='static')
+
+app = Flask (__name__)
+@app.route('/')
+def home():
+        return render_template('Home1.html')
+
+@app.route ('/about')
+def about():
+    return render_template('about.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
