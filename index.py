268178cc0936965bf88386ec09422fa2545936eb
kerdwin <117604921+kerdwin@users.noreply.github.com>
2022-11-08 01:00:46 -0400

    primero

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, redirect, url_for,render_template
 
 
-app=Flask(__name__,template_folder='Templates')
+app=Flask(__name__,template_folder='Templates',static_folder='static')
 
 @app.route('/')
 def home():
